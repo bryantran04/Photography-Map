dd49cb788505d86e3e787f753368c2fbf4a85d63
jguevarra <jfg3xs@virginia.edu>
2019-03-02 18:46:30 -0500

    UI updates -- added pages

--- photography/views.py
+++ photography/views.py
@@ -3,10 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """
-    View for the about_page.html
+    View for the index.html
     :param request:
     :return renders the request parameter, html template, and the context list:
     """
     context = {}
     template = 'photography/index.html'
     return render(request, template, context)
+
+def about(request):
+    """
+    View for the about.html
+    :param request:
+    :return renders the request parameter, html template, and the context list:
+    """
+    context = {}
+    template = 'photography/about.html'
+    return render(request, template, context)
+
+def explore(request):
+    """
+    View for the about.html
+    :param request:
+    :return renders the request parameter, html template, and the context list:
+    """
+    context = {}
+    template = 'photography/explore.html'
+    return render(request, template, context)
+
