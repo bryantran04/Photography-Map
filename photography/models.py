0fc42f76c4250087533b7b47d499c118258365c0
Anne Williams <mawilliams098@gmail.com>
2019-03-02 16:36:19 -0500

    location update

--- photography/models.py
+++ photography/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    zipcode = models.IntegerField()
+    date_posted = models.DateTimeField(default=timezone.now) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+#class Location(models.Model)
+    # for printing in the shell 
+    # def __str__(self):
+    #     return self.title
