0d0efe78632dc2931dbc5b680e85307e39a6e032
Bryan Tran <bryan@Bryans-MacBook-Pro.local>
2019-03-02 16:34:51 -0500

    cloud

--- photography/views.py
+++ photography/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="auth.json"
+
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+import six
+
+
+def sample_analyze_sentiment(content):
+
+    client = language_v1.LanguageServiceClient()
+
+    #content = 'GOOD'
+
+    if isinstance(content, six.binary_type):
+        content = content.decode('utf-8')
+
+    type_ = enums.Document.Type.PLAIN_TEXT
+    document = {'type': type_, 'content': content}
+
+    response = client.analyze_sentiment(document)
+    sentiment = response.document_sentiment
+    return('Score: {}'.format(sentiment.score))
